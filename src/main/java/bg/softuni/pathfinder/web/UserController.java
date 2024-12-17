0074f0690824fb8f01b3b705522a738685c968cf
Teodor_Georgiev <heashoter882@Gmail.com>
2024-12-17 16:27:21 +0200

    Added Lombok

--- src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -3,6 +3,7 @@ package bg.softuni.pathfinder.web;
 import bg.softuni.pathfinder.web.dto.UserRegisterDTO;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -12,7 +13,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class UserController {
 
     @GetMapping("/register")
-    public String viewRegister() {
+    public String viewRegister(Model model) {
+        model.addAttribute("registerData", new UserRegisterDTO());
+
         return "register";
     }
 
