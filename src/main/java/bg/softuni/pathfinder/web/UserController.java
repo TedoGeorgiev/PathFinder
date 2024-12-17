e9c94def5586724c96463394c52641878ca494d7
Teodor_Georgiev <heashoter882@Gmail.com>
2024-12-17 16:59:39 +0200

    Revert "Added Lombok"
    This reverts commit 0074f0690824fb8f01b3b705522a738685c968cf.

--- src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -3,7 +3,6 @@ package bg.softuni.pathfinder.web;
 import bg.softuni.pathfinder.web.dto.UserRegisterDTO;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -13,9 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class UserController {
 
     @GetMapping("/register")
-    public String viewRegister(Model model) {
-        model.addAttribute("registerData", new UserRegisterDTO());
-
+    public String viewRegister() {
         return "register";
     }
 
